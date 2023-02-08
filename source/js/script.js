9f423d3a0b22546f2042fa8452f1cc9351b96781
Anninka <annettech@yandex.ru>
2023-02-08 23:27:49 +1000

    Промежуточные изменения

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,24 @@
-/* в этот файл добавляет скрипты*/
+
+/* BURGER-MENU */
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+/* SWIPER */
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
