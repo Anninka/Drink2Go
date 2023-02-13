b567ae79a0fe40e37a7935bef12798ead659ce29
Anninka <annettech@yandex.ru>
2023-02-13 18:19:24 +1000

    Обновляет данные для gh-pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-import"./menu_mobile.js";import"./slider.js";import"./range_controls.js";import"./map.js";
+import"./menu_mobile.js";import"./slider.js";import"./range_controls.js";import"./sort.js";import"./map.js";
