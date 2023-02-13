bd6f42ff4cecbc2d4ccd21924048f44c226b2e83
Anninka <annettech@yandex.ru>
2023-02-13 19:19:25 +1000

    Обновляет данные для gh-pages

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,1 @@
-const swiper=new Swiper(".swiper",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0},keyboard:{enabled:!0,onlyInViewport:!0},autoplay:{delay:3e3,stopOnLastSLide:!1,disableOnIteraction:!0}});
+const swiper=new Swiper(".swiper",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0},keyboard:{enabled:!0,onlyInViewport:!0},autoplay:{delay:6e3,stopOnLastSLide:!1,disableOnIteraction:!0}});
